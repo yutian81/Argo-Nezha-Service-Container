f699e69672538357cf129b982b56249be6d67360
Kiritocyz <Kiritocyz@users.noreply.github.com>
2025-04-12 16:11:31 +0800

    change grpcpot in init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ if [ ! -s /etc/supervisor/conf.d/damon.conf ]; then
   # 设置 Github CDN 及若干变量，如是 IPv6 only 或者大陆机器，需要 Github 加速网，可自行查找放在 GH_PROXY 处 ，如 https://ghproxy.lvedong.eu.org/ ，能不用就不用，减少因加速网导致的故障。
   GH_PROXY='https://ghproxy.lvedong.eu.org/'
   GRPC_PROXY_PORT=443
-  GRPC_PORT=5555
+  GRPC_PORT=8008
   WEB_PORT=8080
   PRO_PORT=${PRO_PORT:-'80'}
   CADDY_HTTP_PORT=2052
